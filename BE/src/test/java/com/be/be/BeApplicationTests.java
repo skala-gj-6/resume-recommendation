0410f16af58fc8dca93ae441040dba3d23d1caf4
huizoo <ajtwlsgmlwn@naver.com>
2026-09-03 18:56:35 +0900

    refactor(be): migrate Spring configuration to YAML (#20)

--- BE/src/test/java/com/be/be/BeApplicationTests.java
+++ BE/src/test/java/com/be/be/BeApplicationTests.java
@@ -2,8 +2,10 @@ package com.be.be;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BeApplicationTests {
 
 	@Test
