3b4fb51ca8ff98814cfafced6cc4435fcdeb4d4e
huizoo <ajtwlsgmlwn@naver.com>
2026-09-03 18:55:53 +0900

    refactor(be): migrate Spring configuration to YAML

--- BE/src/test/java/com/be/be/CompanySeedIntegrationTests.java
+++ BE/src/test/java/com/be/be/CompanySeedIntegrationTests.java
@@ -6,10 +6,12 @@ import com.be.be.seed.CompanySeedService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CompanySeedIntegrationTests {
 
     @Autowired
